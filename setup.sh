647f33ecea18df470e0eee148e5c5fdb95a49ecd
Alexey Shevchenko <thefelikz@gmail.com>
2014-02-12 17:42:31 +0200

    Remove useless comments

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ TARGET_FILE=$(dirname ${BASH_SOURCE[0]})
 cd `dirname $TARGET_FILE`
 TARGET_FILE=`basename $TARGET_FILE`
 
-# Iterate down a (possible) chain of symlinks
 while [ -L "$TARGET_FILE" ]
 do
     TARGET_FILE=`readlink $TARGET_FILE`
@@ -14,12 +13,9 @@ do
     TARGET_FILE=`basename $TARGET_FILE`
 done
 
-# Compute the canonicalized name by finding the physical path
-# for the directory we're in and appending the target file.
 PHYS_DIR=`pwd -P`
 DIR=$PHYS_DIR/$TARGET_FILE
 
-echo $DIR
 echo "Setting up environment..."
 alias vim="vim -u $DIR/.vimrc"
 echo "Done."
