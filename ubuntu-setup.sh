dead551cb2870e0780d6393a4260963944e84bcc
FelikZ <thefelikz@gmail.com>
2014-09-24 11:17:06 +0000

    - Fix ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,5 +1,6 @@
 sudo apt-get remove vim vim-runtime gvim vim-tiny vim-common vim-gui-common
 sudo apt-get install lua5.1 liblua5.1-dev libncurses5-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev libatk1.0-dev make
+
 wget -O "vim-7.4.tar.bz2" "ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2"
 tar -jxf "vim-7.4.tar.bz2"
 cd "vim74"
@@ -14,4 +15,5 @@ cd "vim74"
 make VIMRUNTIMEDIR=/usr/share/vim/vim74
 sudo make install
 
-rm ../"vim-7.4.tar.bz2"
+cd ".."
+rm -Rf "vim74" "vim-7.4.tar.bz2"
