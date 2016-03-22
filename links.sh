e64c1248966c732907111fd9317c58054ba62ea3
Alexey Shevchenko <thefelikz@gmail.com>
2016-03-22 15:33:57 +0100

    Add links.sh

---
+++ links.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+if [ "$BASH_SOURCE" != "" ]
+then
+    DIR="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
+elif [ "$DASH_SOURCE" != "" ]
+then
+    DIR="$( cd "$( dirname "$DASH_SOURCE" )" && pwd )"
+else
+    DIR="$( cd "$( dirname "$0" )" && pwd )"
+fi
+
+ln -sf "$DIR/.bash_profile_default" "$HOME/.bash_profile_default"
+ln -sf "$DIR/.dircolors" "$HOME/.dircolors"
+ln -sf "$DIR/.tmux.conf" "$HOME/.tmux.conf"
+
+cat $HOME/.bashrc | grep -q "source\ ${HOME}/.bash_profile_default"
+NOT_FOUND=$?
+
+echo $NOT_FOUND
+if [ $NOT_FOUND = 1 ]; then
+    echo "source $HOME/.bash_profile_default" >> "$HOME/.bashrc"
+fi
+
+cat $HOME/.bashrc | grep -q "source\ ${DIR}/light-setup.sh"
+NOT_FOUND=$?
+
+echo $NOT_FOUND
+if [ $NOT_FOUND = 1 ]; then
+    echo "source ${DIR}/light-setup.sh" >> "$HOME/.bashrc"
+fi
