423c53b5b239ecb628e675b0a73df756a310dae4
FelikZ <thefelikz@gmail.com>
2016-04-10 11:06:13 +0000

    Fix Cannot add PPA
    See:
    http://askubuntu.com/questions/429803/cannot-add-ppa-please-check-that-the-ppa-name-or-format-is-correct

--- ubuntu-tmux-setup.sh
+++ ubuntu-tmux-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 # http://stackoverflow.com/questions/25940944/ugrade-tmux-from-1-8-to-1-9-on-ubuntu-14-04
+sudo apt-get install --reinstall ca-certificates
 sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get update
 sudo apt-get install -y tmux
