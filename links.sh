f3d71a5fda0c4d146128983b600b3c0a1f1efd3e
Alexey Shevchenko <thefelikz@gmail.com>
2017-03-21 15:09:57 +0100

    Add json alias and .ideavimrc

--- links.sh
+++ links.sh
@@ -13,6 +13,7 @@ fi
 ln -sf "$DIR/.bash_profile_default" "$HOME/.bash_profile_default"
 ln -sf "$DIR/.dircolors" "$HOME/.dircolors"
 ln -sf "$DIR/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$DIR/.ideavimrc" "$HOME/.ideavimrc"
 
 cat $HOME/.bashrc | grep -q "source\ ${HOME}/.bash_profile_default"
 NOT_FOUND=$?
