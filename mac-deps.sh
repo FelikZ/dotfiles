d3d0cc7935743727563fa6273c1f2e3e20793cd3
Oleksii Shevchenko <thefelikz@gmail.com>
2018-04-06 11:50:13 +0200

    Add mac-deps.sh / Comment recycle sockets fix

---
+++ mac-deps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+brew tap discoteq/discoteq
+brew install flock
--- sockets.sh
+++ sockets.sh
@@ -2,5 +2,5 @@
 
 sysctl -w net.ipv4.tcp_keepalive_time=600
 sysctl -w net.ipv4.tcp_tw_reuse=1
-sysctl -w net.ipv4.tcp_tw_recycle=1
+# sysctl -w net.ipv4.tcp_tw_recycle=1
 sysctl -w "net.ipv4.ip_local_port_range=1025 61000"
