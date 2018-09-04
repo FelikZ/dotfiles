e5285772969b553c7926f6d28797b87f66220852
Alexey Shevchenko <thefelikz@gmail.com>
2018-09-04 10:35:52 +0200

    Fix mac setup

--- mac-deps.sh
+++ mac-deps.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 brew tap discoteq/discoteq
-brew install flock
+brew install flock \
+    git \
+    bash-completion
+brew cask install osxfuse
+brew install encfs
+
+# install software
+# http://www.usboverdrive.com/USBOverdrive/Download.html - mouse accel fix
+# http://triq.net/articles/mouse-acceleration-download
+# defaults write .GlobalPreferences com.apple.mouse.scaling -1
+# defaults write -g KeyRepeat -int 1
+# defaults write -g InitialKeyRepeat -int 1
+# defaults write -g ApplePressAndHoldEnabled -bool false
+# Dropbox
+# Karabiner elements - ` and caps lock fix
