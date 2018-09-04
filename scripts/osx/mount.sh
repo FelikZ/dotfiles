9dc3445407ea5245cdb38e7fc1d41c1b5a71bfd1
Alexey Shevchenko <thefelikz@gmail.com>
2018-09-04 10:42:04 +0200

    Fix mount.sh

--- scripts/osx/mount.sh
+++ scripts/osx/mount.sh
@@ -4,3 +4,6 @@ sudo chown felikz /Volumes/Work
 encfs /Users/felikz/Dropbox/Work /Volumes/Work
 chmod -R 0700 /Volumes/Work/Secure/.ssh/
 find /Volumes/Work/Secure/.ssh -type d | xargs chmod +x
+unlink ~/.ssh || true
+rmdir ~/.ssh || true
+ln -sf /Volumes/Work/Secure/.ssh ~
