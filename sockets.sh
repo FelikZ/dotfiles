4a63c7054fe692d24dfb4b8b9c02401c7b3941c5
Alexey Shevchenko <thefelikz@gmail.com>
2018-12-18 16:18:10 +0100

    Add docker-ips alias

--- sockets.sh
+++ sockets.sh
@@ -4,3 +4,7 @@ sysctl -w net.ipv4.tcp_keepalive_time=600
 sysctl -w net.ipv4.tcp_tw_reuse=1
 # sysctl -w net.ipv4.tcp_tw_recycle=1
 sysctl -w "net.ipv4.ip_local_port_range=1025 61000"
+
+# Investigate
+# net.core.somaxconn = 128
+# net.ipv4.tcp_max_syn_backlog = 512
