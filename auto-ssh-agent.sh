e19021307a8f7aaec8d51a82c5b30ef80d5a1bce
Alexey Shevchenko <thefelikz@gmail.com>
2019-11-03 18:53:20 +0100

    Fix agent forwarding

--- auto-ssh-agent.sh
+++ auto-ssh-agent.sh
@@ -3,9 +3,13 @@
 HOSTNAME=$(hostname)
 SHARED_AUTH_SOCK="$(cd ~ && pwd)/ssh-auth-sock.$HOSTNAME"
 
-if [ ! -S "$SHARED_AUTH_SOCK" ] || [ `ps aux | grep ssh-agent | wc -l` = 0 ]; then
-    eval `ssh-agent`
+if [ -n "$SSH_AUTH_SOCK" ]; then
+    return
+fi
+
+if [ ! -S "$SHARED_AUTH_SOCK" ] || [ $(ps aux | grep ssh-agent | wc -l) = 0 ]; then
+    eval $(ssh-agent)
     ln -sf "$SSH_AUTH_SOCK" "$SHARED_AUTH_SOCK"
 fi
 export SSH_AUTH_SOCK=$SHARED_AUTH_SOCK
-ssh-add -l > /dev/null || ssh-add
+ssh-add -l >/dev/null || ssh-add
