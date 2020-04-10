be57ffb39a82388c237644bc95ae59f7e64eff7f
Alexey Shevchenko <thefelikz@gmail.com>
2020-04-10 19:30:45 +0200

    Better WSL support

--- scripts/win/mount.sh
+++ scripts/win/mount.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo unlink "$HOME/.ssh" 2>/dev/null || true
-sudo umount /mnt/z || true
-sudo mkdir -p /mnt/z
-sudo mount -t drvfs -o metadata,uid=1000,gid=1000,umask=22,fmask=155 'Z:\' /mnt/z
-sudo ln -sf /mnt/z/.ssh "$HOME/.ssh"
+sudo umount /z || true
+sudo mkdir -p /z
+sudo mount -t drvfs -o metadata,uid=1000,gid=1000,umask=22,fmask=155 'Z:\' /z
+sudo ln -sf /z/.ssh "$HOME/.ssh"
--- win-wsl-setup.sh
+++ win-wsl-setup.sh
@@ -2,12 +2,13 @@
 
 sudo bash -c 'cat <<EOF >/etc/wsl.conf
 [automount]
+root = /
 options = "metadata"
 
 EOF'
 
-ln -sf /mnt/c/Users/Home/dotfiles ~/dotfiles
-ln -sf /mnt/c/Users/Home/dotfiles/scripts/win/mount.sh ~/mount.sh
+ln -sf /c/Users/Home/dotfiles ~/dotfiles
+ln -sf /c/Users/Home/dotfiles/scripts/win/mount.sh ~/mount.sh
 chmod +x ~/mount.sh
 
 source ~/dotfiles/ubuntu-setup.sh
