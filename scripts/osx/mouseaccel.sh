d252ab6333dcffd127d02f2b893ba2202485b3b5
Alexey Shevchenko <thefelikz@gmail.com>
2021-02-17 14:54:32 +0100

    Update mouseaccel

--- scripts/osx/mouseaccel.sh
+++ scripts/osx/mouseaccel.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 defaults write -g com.apple.mouse.scaling -1
+
+echo "If you running Gaming mouse, you have follow this:"
+echo "https://apple.stackexchange.com/a/384227/34773"
